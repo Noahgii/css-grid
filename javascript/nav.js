ef9862991189e611794d26911bdd6d943e3dbe7e
Noahgii <enzo.ken.ts@gmail.com>
2023-12-13 17:44:40 -0300

    foi

--- javascript/nav.js
+++ javascript/nav.js
@@ -2,9 +2,9 @@ const nav = document.querySelector("nav");
 
 nav.innerHTML = `
 <ul>
-<li><a href="../index.html">Início</a></li>
-<li><a href="../html/gridumacoluna.html">Grid Simples - Uma Coluna</a></li>
-<li><a href="../html/gridvariascolunas.html">Grid simples - Várias colunas </a></li>
-<li><a href="../html/GridColunasAdaptaveis.html">Grid Adaptavel</a></li>
+<li><a href="/index.html">Início</a></li>
+<li><a href="/gridumacoluna.html">Grid Simples - Uma Coluna</a></li>
+<li><a href="/gridvariascolunas.html">Grid simples - Várias colunas </a></li>
+<li><a href="/GridColunasAdaptaveis.html">Grid Adaptavel</a></li>
 </ul>
 `;
