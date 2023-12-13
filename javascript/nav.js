75d9a649b9af966df5591aaec8dc4e5867210a2b
Noahgii <enzo.ken.ts@gmail.com>
2023-12-13 16:45:17 -0300

    corrigir experiencias

--- javascript/nav.js
+++ javascript/nav.js
@@ -6,7 +6,5 @@ nav.innerHTML = `
 <li><a href="/html/gridumacoluna.html">Grid Simples - Uma Coluna</a></li>
 <li><a href="/html/gridvariascolunas.html">Grid simples - Várias colunas </a></li>
 <li><a href="/html/GridColunasAdaptaveis.html">Grid Adaptavel</a></li>
-<li><a href="#">Experiência 04</a></li>
-<li><a href="#">Experiência 05</a></li>
 </ul>
 `;
